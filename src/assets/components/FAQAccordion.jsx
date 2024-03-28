d29b0f8b23e0a40636842e52d72feeeb0a43a159
Om Preetham <ompreethambandi@gmail.com>
2024-03-27 19:07:42 -0500

    updated semantic html

--- src/assets/components/FAQAccordion.jsx
+++ src/assets/components/FAQAccordion.jsx
@@ -15,44 +15,40 @@ const FAQAccordion = () => {
   }
 
   return (
-    <div>
-      <main>
-        <div className="wrapper">
-          <section>
-            <div className="layout-flex">
-              <div className="section-heading">
-                <h1 dataIcon="star">FAQs</h1>
-              </div>
-              <div className="faq-wrapper">
-                {data.map((faq, index) => {
-                  return (
-                    <article key={index}>
-                      <div className={`faq${faq.id}-wrapper`}>
-                        <div>
-                          <h2>{faq.question}</h2>
-                          <img
-                            onClick={() => toggleFAQ(index)}
-                            src={`/images/${
-                              visibleItems[index] ? 'icon-minus' : 'icon-plus'
-                            }.svg`}
-                            alt="Plus Icon"
-                          />
-                        </div>
-                        <p
-                          className={visibleItems[index] ? 'visible' : 'hidden'}
-                        >
-                          {faq.answer}
-                        </p>
-                      </div>
-                    </article>
-                  )
-                })}
-              </div>
+    <main>
+      <div className="wrapper">
+        <section>
+          <div className="layout-flex">
+            <div className="section-heading">
+              <h1 dataIcon="star">FAQs</h1>
             </div>
-          </section>
-        </div>
-      </main>
-    </div>
+            <div className="faq-wrapper">
+              {data.map((faq, index) => {
+                return (
+                  <article key={index}>
+                    <div className={`faq${faq.id}-wrapper`}>
+                      <div>
+                        <h2>{faq.question}</h2>
+                        <img
+                          onClick={() => toggleFAQ(index)}
+                          src={`/images/${
+                            visibleItems[index] ? 'icon-minus' : 'icon-plus'
+                          }.svg`}
+                          alt="Plus Icon"
+                        />
+                      </div>
+                      <p className={visibleItems[index] ? 'visible' : 'hidden'}>
+                        {faq.answer}
+                      </p>
+                    </div>
+                  </article>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 export default FAQAccordion
